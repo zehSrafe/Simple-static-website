f15f9ebf7f78bb05bba62f0d7b231fd432dfc1d1
Loïc Deketelaere <zehsrafe@gmail.com>
2022-03-31 15:40:48 +0200

    Enhanced and expanded

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,16 @@
-function myMessage() {
-    alert("Well well well looks like someone got rickrolled!");
+let timeout;
+
+function myFunction() {
+  var vid = document.getElementById("myVideo");
+  var surpriseText = document.getElementById("surprise-enabler");
+
+  surpriseText.style.display = "none";
+  vid.style.display = "block";
+  vid.play();
+
+  timeout = setTimeout(alertFunc, 1000);
+}
+
+function alertFunc() {
+  alert("Well well well looks like someone got rickrolled!");
 }
